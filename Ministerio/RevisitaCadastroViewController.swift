5f7024493ae9e926acde48fcecf9b504e7d88652
DEIVID LOUREIRO <Deivid@macbook-air-de-deivid.local>
2016-12-16 19:15:25 -0200

    Campos adicionados. Falta auto layout.

--- Ministerio/RevisitaCadastroViewController.swift
+++ Ministerio/RevisitaCadastroViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class RevisitaCadastroViewController: UIViewController {
 
+    @IBOutlet weak var txtNotas: UITextView!
+    @IBOutlet weak var sldFonte: UISlider!
+    
+    let revisitaNotasFonte = "RevisitaNotasFonte"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.alterarTamanhoFonteNotas(tamanho: self.recuperarTamanhoFonte())
+        self.sldFonte.value = Float(self.recuperarTamanhoFonte())
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,4 +34,28 @@ class RevisitaCadastroViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
 
+    @IBAction func alterarFonteNotas(_ sender: Any) {
+        let size = CGFloat(sldFonte.value)
+        self.alterarTamanhoFonteNotas(tamanho: size)
+        self.salvarTamanhoFonte(tamanho: size)
+    }
+    
+    //MARK: FUNCOES
+    
+    func salvarTamanhoFonte(tamanho: CGFloat) {
+        UserDefaults.standard.set(tamanho, forKey: self.revisitaNotasFonte)
+    }
+    
+    func recuperarTamanhoFonte() -> CGFloat{
+        if let tamanhoRecuperado = UserDefaults.standard.object(forKey: self.revisitaNotasFonte){
+            return tamanhoRecuperado as! CGFloat
+        }else{
+            self.salvarTamanhoFonte(tamanho: 14)
+            return 14
+        }
+    }
+    
+    func alterarTamanhoFonteNotas(tamanho: CGFloat) {
+        txtNotas.font = UIFont(name: (txtNotas.font?.fontName)!, size: tamanho)
+    }
 }
