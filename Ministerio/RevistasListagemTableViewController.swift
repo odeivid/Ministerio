2b7cc6214756c89a2fcfa1e4bd537b7605acaf22
DEIVID LOUREIRO <Deivid@macbook-air-de-deivid.local>
2016-12-20 18:32:52 -0200

    Listando as revisitas

---
+++ Ministerio/RevistasListagemTableViewController.swift
@@ -0,0 +1,110 @@
+//
+//  RevistasListagemTableViewController.swift
+//  Ministerio
+//
+//  Created by DEIVID LOUREIRO on 20/12/16.
+//  Copyright © 2016 DEIVID LOUREIRO. All rights reserved.
+//
+
+import UIKit
+
+class RevistasListagemTableViewController: UITableViewController {
+
+    var revisitas: [Revisita] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.carregarRevisitas()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        self.carregarTabela()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return revisitas.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "celula", for: indexPath)
+
+        cell.textLabel?.text = revisitas[indexPath.row].nome
+
+        return cell
+    }
+    
+    // MARK: - METODOS
+    func carregarRevisitas() {
+        revisitas = CoreDataRevisita().getRevisitas(ativoSimNao: true)
+    }
+    
+    func carregarTabela() {
+        self.revisitas = CoreDataRevisita().getRevisitas(ativoSimNao: true)
+        tableView.reloadData()
+    }
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
