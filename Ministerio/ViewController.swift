20e577d237150628626b4969263dfe7da9fcfc56
DEIVID LOUREIRO <Deivid@macbook-air-de-deivid.local>
2017-01-05 22:57:36 -0200

    Adicionado link para site jw e app jw library.

--- Ministerio/ViewController.swift
+++ Ministerio/ViewController.swift
@@ -24,6 +24,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //MARK - ACTIONS
+    @IBAction func abrirSiteJW(_ sender: Any) {
+        if let siteURL = NSURL(string: "http://www.jw.org") {
+            
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(siteURL as URL, options: [:], completionHandler: nil)
+            } else {
+                UIApplication.shared.openURL(siteURL as URL)
+            }
+        }
+    }
+    
+    @IBAction func abrirAppJW(_ sender: Any) {
+        
+        if let appURL = URL(string: "jwlibrary://"){
+            
+            if UIApplication.shared.canOpenURL(appURL){
+            
+                if #available(iOS 10.0, *) {
+                    UIApplication.shared.open(appURL, options: [:], completionHandler: nil)
+                } else {
+                    UIApplication.shared.openURL(appURL)
+                }
+            }
+        }else{
+         
+            if let siteURL = NSURL(string: "itms://itunes.apple.com/us/app/jw-library/id672417831?ls=1&mt=8") {
+                
+                if #available(iOS 10.0, *) {
+                    UIApplication.shared.open(siteURL as URL, options: [:], completionHandler: nil)
+                } else {
+                    UIApplication.shared.openURL(siteURL as URL)
+                }
+            }
+        }
+    }
+    
 }
 
